7739e229a286335407e1e8fabfac715fa678ef7c
ameena117 <ameenajadallah@gmail.com>
2024-02-15 10:42:07 +0200

    up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef, useState } from "react";
+import "./App.css";
+import "./index.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [todos, setTodos] = useState([]);
+    const inputRef = useRef();
+    const Add = () => {
+        const text = inputRef.current.value;
+        const newItem = text;
+        setTodos([...todos, newItem]);
+        inputRef.current.value = "";
+    };
+
+    const Done = (index) => {
+        document.querySelectorAll(".item")[index].classList.toggle("checked");
+    }
+
+    return (
+        <div className="App">
+            <div className="todo-list">
+                <h2>Todo List</h2>
+                <div className="form">
+                    <input className="inputText" ref={inputRef} placeholder="Todo Item" />
+                    <button className="inputSubmit" onClick={Add}>Add</button>
+                </div>
+                <ul>
+                    {todos.map((text , index) => {
+                        return (
+                            <li className="list">
+                                <input type="checkbox" onChange={() => Done(index)}/>
+                                <li className="item">{text}</li>
+                            </li>
+                        );
+                    })}
+                </ul>
+            </div>
+        </div>
+    );
 }
 
 export default App;
